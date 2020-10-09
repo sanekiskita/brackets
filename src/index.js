da5d9368b0db74fa0278577cff371b21e6aa503e
sanekiskita <as1405503@gmail.com>
2020-10-09 15:47:48 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    console.log(str);
+    console.log(bracketsConfig);
+    var strco = "";
+    for (var i = 0; i <= str.length; i++) {
+        for (var i2 = 0; i2 < bracketsConfig.length; i2++) {
+            for (var i3 = 0; i3 < 2; i3++) {
+                if (str.substr(i, 1) === bracketsConfig[i2][i3]) {
+                    strco = strco + str.substr(i, 1);
+                }
+            }
+            if(  str.split(bracketsConfig[i2][0]).join("").length != str.split(bracketsConfig[i2][1]).join("").length )return false
+        }
+
+
+    }
+
+
+    for (var i = 0; i <= strco.length; i++) {
+        for (var i2 = 0; i2 < bracketsConfig.length; i2++) {
+            if (bracketsConfig[i2][0] !== undefined)
+                res = bracketsConfig[i2][0] + bracketsConfig[i2][1];
+
+            while (strco.includes(res)) {
+                strco = strco.split(res).join("");
+            }
+        }
+   }
+    if (strco === "")
+        return true;
+    else return false;
+
 }
