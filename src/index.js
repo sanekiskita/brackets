ecc5090352179a1aab4698319b9d4dcd72f1c77a
sanekiskita <as1405503@gmail.com>
2020-10-09 18:30:34 +0300

    fix: bug

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-    console.log(str);
-    console.log(bracketsConfig);
+    //console.log(str);  console.log(bracketsConfig);
     var strco = "";
     for (var i = 0; i <= str.length; i++) {
         for (var i2 = 0; i2 < bracketsConfig.length; i2++) {
             for (var i3 = 0; i3 < 2; i3++) {
                 if (str.substr(i, 1) === bracketsConfig[i2][i3]) {
                     strco = strco + str.substr(i, 1);
+                    break;
                 }
             }
-            if(  str.split(bracketsConfig[i2][0]).join("").length != str.split(bracketsConfig[i2][1]).join("").length )return false
         }
-
-
     }
 
 
@@ -23,9 +20,10 @@ module.exports = function check(str, bracketsConfig) {
 
             while (strco.includes(res)) {
                 strco = strco.split(res).join("");
+                i--;
             }
         }
-   }
+    }
     if (strco === "")
         return true;
     else return false;
